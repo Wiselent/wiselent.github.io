fada21cdbe900e49c3a437ad33b766e02e2e7f1a
Alan Dong <alan@blacksheep.ws>
2015-09-09 23:41:58 -0700

    init

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(function(){
+  var currentDate = new Date();
+  var pastDate  = new Date(currentDate.getFullYear(), 10, 15);
+  var diff = Math.abs(currentDate.getTime() / 1000 - pastDate.getTime() / 1000);
+
+  $('.countdown-clock').FlipClock(diff, {
+    clockFace: 'DailyCounter',
+    countdown: true,
+		classes: {
+			active: 'flip-clock-active',
+			before: 'flip-clock-before',
+			divider: 'flip-clock-divider',
+			dot: 'flip-clock-dot',
+			// label: 'flip-clock-label',
+			label: 'hidden',
+			flip: 'flip',
+			play: 'play',
+			wrapper: 'flip-clock-wrapper'
+		},
+  });
+});
