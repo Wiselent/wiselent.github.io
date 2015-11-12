89d245211a1d03ffce184c120186a45c04b4e2d2
Lin Dong <ldong@users.noreply.github.com>
2015-11-11 22:41:39 -0800

    Update app.js
    Add 45 days from now.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function(){
   var currentDate = new Date();
-  var pastDate  = new Date(currentDate.getFullYear(), 10, 15);
+  var pastDate  = new Date(currentDate.getFullYear(), 12, 27);
   var diff = Math.abs(currentDate.getTime() / 1000 - pastDate.getTime() / 1000);
 
   $('.countdown-clock').FlipClock(diff, {
